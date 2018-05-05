cf98b6f6845e4d78cf9ecddf8f453be48e435731
neerajUdai <blockm_deployment>
2018-05-05 18:20:16 +0530

    24 hr %

--- binance-bots/binance_ui.php
+++ binance-bots/binance_ui.php
@@ -56,10 +56,15 @@ $user_name = $_POST['selectApiKeys'];
     $exchange->apiKey = $binanceApisapisapis[$user_name][Key];
     $exchange->secret = $binanceApisapisapis[$user_name][Secret];
 }
+//
+//$start_range = '2018-04-01 00:00:00';
+//$start_range_for_binance = strtotime($start_range).'000';
 
-
+//$open_orders_9 = $exchange->fetch_open_orders('LEND/BTC');
+//$open_orders_9 = $exchange->fetch_open_orders(null,$start_range_for_binance);
 
 $balance = $exchange->fetch_balance ();
+//$balance = $exchange->fetch_markets()
 //$closed_orders = $exchange->fetch_closed_orders();
 $tickers = $exchange->fetch_tickers_on_quote('btc');
 $coins_with_positive_balance = array();
@@ -76,6 +81,7 @@ foreach($coins_with_positive_balance as $key => $value){
     if(strtolower($key) == 'btc'){
         $coins_with_positive_balance[$key]['current_price'] = 1;
         $coins_with_positive_balance[$key]['avg_buy_price'] = 1;
+        $coins_with_positive_balance[$key]['last_24hr_percentage_change'] = 1;
         $coins_with_positive_balance[$key]['btc_value_of_coin'] = $value[quantity];
         $total_bitcoins = $value[quantity];
     }
@@ -83,6 +89,7 @@ foreach($coins_with_positive_balance as $key => $value){
         $coins_with_positive_balance[$key]['current_price'] = $tickers[$key][last];
         $coins_with_positive_balance[$key]['btc_value_of_coin'] = $tickers[$key][last]*$value[quantity];
         $coins_with_positive_balance[$key]['symbol'] = $tickers[$key][symbol];
+        $coins_with_positive_balance[$key]['last_24hr_percentage_change'] = $tickers[$key][percentage];
         $symbol_closed_orders = $exchange->fetch_closed_orders($tickers[$key][symbol]);
         $total_coin_amount = 0;
         $total_coin_cost = 0;
@@ -114,6 +121,7 @@ echo "
                     <th>Quantity</th>
                     <th>Avg Buying Price</th>
                     <th>Current Price</th>
+                    <th>Last 24 Hr Price Change</th>
                     <th>Profit %</th>
                     <th>BTC Value</th>
                     <th>BTC Cost</th>
@@ -124,13 +132,16 @@ echo "
 
 foreach ($coins_with_positive_balance as $key => $value){
     $percentage_profit = round(($value[current_price] - $value[avg_buy_price])/$value[avg_buy_price],4)*100;
+    if(!$value[current_price])
+        $percentage_profit = -1000;
     echo        " 
                     <tr>
                         <td>$key</td>
                         <td>$value[quantity]</td>
                         <td>".number_format($value[avg_buy_price],10)."</td>
                         <td>".number_format($value[current_price],10)."</td>
-                        <td>".number_format($percentage_profit,10)." %</td>
+                        <td>".number_format($value[last_24hr_percentage_change],2)."</td>
+                        <td>".number_format($percentage_profit,2)."</td>
                         <td>".number_format($value[btc_value_of_coin],10)."</td>
                         <td>".number_format($value[total_btc_expenditure],10)."</td>
                   </tr>
@@ -160,6 +171,7 @@ echo "<table  class = 'table table-hover' id = 'portfolioTotal'>
                         <td>Total</td>
                         <td>Total</td>
                         <td>Total</td>
+                        <td>Total</td>
                         <td>$total_bitcoins</td>
                         <td>$total_btc_spent</td>
                   </tr></tbody></table>
@@ -174,6 +186,9 @@ echo "<table  class = 'table table-hover' id = 'portfolioTotal'>
 //$open_orders_6 = $exchange->fetch_open_orders('WINGS/BTC');
 //$open_orders_7 = $exchange->fetch_open_orders('HSR/BTC');
 //$open_orders_7 = $exchange->fetch_open_orders('QSP/BTC');
+//$open_orders_8 = $exchange->fetch_open_orders('ICN/BTC');
+//$open_orders_9 = $exchange->fetch_open_orders('LEND/BTC');
+//$open_orders_9 = $exchange->fetch_open_orders();
 ////echo "asd";
 ////exit;
 //$create_order = $exchange->create_limit_buy_order('GAS/BTC',10,0.003011);
@@ -186,7 +201,10 @@ echo "<table  class = 'table table-hover' id = 'portfolioTotal'>
 ////$create_order = $exchange->create_limit_buy_order('WINGS/BTC',665,0.0000621);
 ////$create_order = $exchange->create_limit_buy_order('HSR/BTC',45,0.001125);
 //$create_order = $exchange->create_limit_buy_order('QSP/BTC',4625,0.00002175);
+//$create_order = $exchange->create_market_buy_order('QSP/BTC',4625,0.00002175);
 //$create_order = $exchange->create_limit_sell_order('QSP/BTC',4625,0.00002180);
+//$create_order = $exchange->create_limit_sell_order('ICN/BTC',583,0.00017130);
+//$create_order = $exchange->create_limit_buy_order('LEND/BTC',5000,0.00000550);
 //
 //$open_orders_2 = $exchange->fetch_open_orders('GAS/BTC');
 
@@ -196,6 +214,7 @@ echo "<table  class = 'table table-hover' id = 'portfolioTotal'>
 //$open_orders_6_C = $exchange->fetch_open_orders('WINGS/BTC');
 //$open_orders_7_C = $exchange->fetch_open_orders('HSR/BTC');
 //$open_orders_7c = $exchange->fetch_open_orders('QSP/BTC');
+//$open_orders_9c = $exchange->fetch_open_orders('LEND/BTC');
 
 
 
